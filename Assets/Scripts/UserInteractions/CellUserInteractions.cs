cabf6bac47a2c1cde44fbfd21f0847176df4894f
Delfeil <delfeilcasanova@gmail.com>
2021-03-12 23:51:52 +0100

    [Feature] Cell colors depending on mouse

---
+++ Assets/Scripts/UserInteractions/CellUserInteractions.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellUserInteractions : MonoBehaviour
+{
+    public Material defaultMat;
+    public Material overMat;
+    public Material clickedMat;
+    Renderer render;
+    CellStates state = CellStates.Unselected;
+
+    private void Start()
+    {
+        render = gameObject.GetComponent<Renderer>();
+    }
+
+    private void OnMouseOver()
+    {
+        if(state != CellStates.Selected)
+            render.material = overMat;
+    }
+
+    private void OnMouseExit()
+    {
+        if(state != CellStates.Selected)
+            render.material = defaultMat;
+    }
+
+    private void OnMouseUp()
+    {
+        render.material = clickedMat;
+        state = CellStates.Selected;
+    }
+}
