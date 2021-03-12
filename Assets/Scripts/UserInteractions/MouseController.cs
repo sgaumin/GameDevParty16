4160923c91bc9e508ffd8404c5d407d7734a6656
Delfeil <delfeilcasanova@gmail.com>
2021-03-12 23:02:41 +0100

    Auto stash before rebase of "origin/master"
    [Cells] Click on cells

---
+++ Assets/Scripts/UserInteractions/MouseController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+    public Camera cam;
+    public LayerMask terrain;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            // Left Click
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, terrain))
+            {
+                // click on cell
+                hit.transform.gameObject.GetComponent<Renderer>().material.color = Color.red;
+            }
+        }
+    }
+}
