0aad3173392aab7300c0a053d8744ffa50d9cbd0
Sebastien Gm <sgaumin@gmail.com>
2021-03-15 01:47:57 +0900

    [Feature] Video set

---
+++ Assets/Scripts/LoadSceneAfterDuration.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using Tools;
+using UnityEngine;
+
+public class LoadSceneAfterDuration : MonoBehaviour
+{
+	void Start()
+	{
+		StartCoroutine(Core());
+	}
+
+	private IEnumerator Core()
+	{
+		yield return new WaitForSeconds(45f);
+		LevelLoader.LoadNextLevel();
+	}
+}
