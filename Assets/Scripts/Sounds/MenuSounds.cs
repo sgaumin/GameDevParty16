e1a31531b5e9fdff12d52b7c61c99e879dae82f0
Delfeil <delfeilcasanova@gmail.com>
2021-03-14 16:47:57 +0100

    [Feature] Sound on demarrage

---
+++ Assets/Scripts/Sounds/MenuSounds.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSounds : MonoBehaviour
+{
+    public SoundData startSound;
+
+    public void PlayStarSound()
+    {
+        startSound.instrument.Play();
+        startSound.voice.Play();
+    }
+}
--- Assets/Scripts/Sounds/SoundData.cs
+++ Assets/Scripts/Sounds/SoundData.cs
@@ -8,4 +8,20 @@ public class SoundData : ScriptableObject
 {
     public AudioExpress instrument;
     public AudioExpress voice;
+    [ContextMenu("PlaySounds")]
+    public void PlaySounds()
+    {
+        PlayInstrumentSound();
+        PlayVoiceSound();
+    }
+
+    public void PlayInstrumentSound()
+    {
+        instrument.Play();
+    }
+
+    public void PlayVoiceSound()
+    {
+        voice.Play();
+    }
 }
