8023ed12796b482108f36a07440bd43148b79d02
Delfeil <delfeilcasanova@gmail.com>
2021-06-05 16:48:41 +0200

    [Graphics] Intégration ui select level (need fix goods assets)

---
+++ Assets/Scripts/UserInterface/MenuLevelController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using DG.Tweening;
+
+public class MenuLevelController : MonoBehaviour
+{
+    [Header("Params")]
+    [SerializeField] private float scaleFactor = 1.2f;
+
+    public void ScaleUp(GameObject button)
+    {
+        button.transform.DOScale(new Vector3(1, 1, 1) * scaleFactor, 0.5f);
+    }
+
+    public void ScaleDown(GameObject button)
+    {
+        button.transform.DOScale(new Vector3(1.0f, 1.0f, 1.0f), 0.5f);
+    }
+}
