e7c0d8a682d62836374af02843fc8a3daf626d89
Delfeil <delfeilcasanova@gmail.com>
2021-06-05 18:11:26 +0200

    [Feature] main menu to menu levels working

---
+++ Assets/Scripts/UserInterface/MainMenuController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MainMenuController : MonoBehaviour
+{
+    [Header("References")]
+    private GameObject activeGameObject;
+    [SerializeField] private GameObject main;
+    [SerializeField] private GameObject level;
+    private Dictionary<MainMenuState, GameObject> statesObjects;
+
+    [Header("Parameters")]
+    [SerializeField] private MainMenuState state = MainMenuState.Main;
+
+    private void Awake()
+    {
+        statesObjects = new Dictionary<MainMenuState, GameObject>();
+        statesObjects.Add(MainMenuState.Main, main);
+        statesObjects.Add(MainMenuState.Level, level);
+        activeGameObject = statesObjects[state];
+        SwitchState((int)state);
+    }
+
+    public void SwitchState(int stateId)
+    {
+        state = (MainMenuState)stateId;
+        activeGameObject.SetActive(false);
+        activeGameObject = statesObjects[state];
+        activeGameObject.SetActive(true);
+    }
+}
